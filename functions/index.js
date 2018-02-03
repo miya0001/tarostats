6991fd1e1e23b4b678e55be9be7b61abf4454915
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-02-04 00:21:04 +0900

    cs fixes

--- functions/index.js
+++ functions/index.js
@@ -16,8 +16,8 @@ const functions = require( 'firebase-functions' )
 const getStats = require( './lib/getstats' )
 
 exports.checkstats = functions.pubsub.topic( 'checkstats' ).onPublish( ( event ) => {
-    const st = new getStats( 'https://miya.io/' );
-    st.req( ( res ) => {
-      console.log( res )
-    } )
+  const st = new getStats( 'https://miya.io/' );
+  st.req( ( res ) => {
+    console.log( res )
+  } )
 } );
