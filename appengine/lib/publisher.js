9a8468ba57e621f2597489ec33f90b0a94561822
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-02-08 15:59:16 +0900

    some fix for publisher

--- appengine/lib/publisher.js
+++ appengine/lib/publisher.js
@@ -7,39 +7,25 @@ const Publisher = function( projectId, topicName ) {
   this.topicName = topicName;
 }
 
-Publisher.prototype.send = function() {
-  // Your Google Cloud Platform project ID
+Publisher.prototype.send = function( data, callback ) {
   const projectId = this.projectId;
-
-  // Instantiates a client
-  const pubsub = new PubSub({
-    projectId: projectId,
-  });
-
-  // The name for the new topic
   const topicName = this.topicName;
+  const dataBuffer = Buffer.from( data );
 
-  /**
-   * TODO(developer): Uncomment the following lines to run the sample.
-   */
-  // const topicName = 'your-topic';
-  // const data = JSON.stringify({ foo: 'bar' });
-
-  // Publishes the message as a string, e.g. "Hello, world!" or JSON.stringify(someObject)
-  const dataBuffer = Buffer.from('Publish from cron!');
+  const pubsub = new PubSub( {
+    projectId: projectId,
+  } );
 
   pubsub
-    .topic(topicName)
+    .topic( topicName )
     .publisher()
-    .publish(dataBuffer)
-    .then(results => {
-      const messageId = results[0];
-      console.log(`Message ${messageId} published.`);
-    })
-    .catch(err => {
-      console.error('ERROR:', err);
-    });
-// [END pubsub_publish_message]
+    .publish( dataBuffer )
+    .then( results => {
+      callback( results )
+    } )
+    .catch( err => {
+      console.error( 'ERROR:', err );
+    } );
 }
 
 module.exports = Publisher;
